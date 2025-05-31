ba6d008a111b12629ae352534460b4066d3c2dea
vitor0p9f <dev.vitorpfontes002@gmail.com>
2025-05-31 11:52:48 -0300

    feat(adapters): add an OpenGL adapter
    Use the `Idrawer` interface to create an adapter for OpenGL.
    
    Implement the methods to draw `Rooms`, `Hallways`, and `Map`
    entities.

---
+++ adapters/opengl_drawer.cpp
@@ -0,0 +1,56 @@
+#include <GL/freeglut.h>
+#include <GL/gl.h>
+#include <vector>
+#include "opengl_drawer.hpp"
+#include "../domain/game/map/map.hpp"
+
+OpenGLDrawer::OpenGLDrawer(){};
+
+void OpenGLDrawer::drawRoom(const Room& room) const {
+  int x = room.coordinate.x * tile_size;
+  int y = room.coordinate.y * tile_size;
+  int width = room.width * tile_size; 
+  int height = room.height * tile_size;
+
+  glBegin(GL_QUADS);
+    glColor3f(0.0f, 0.0f, 0.0f); // RBG: black
+
+    glVertex2i(x, y);
+    glVertex2i(x + width, y);
+    glVertex2i(x + width, y + height);
+    glVertex2i(x, y + height);
+  glEnd();
+}
+
+void OpenGLDrawer::drawHallway(const Hallway& hallway) const {
+  int x = hallway.coordinate.x * tile_size;
+  int y = hallway.coordinate.y * tile_size;
+  int width = hallway.width * tile_size; 
+  int height = hallway.height * tile_size;
+
+  glBegin(GL_QUADS);
+    glColor3f(0.0f, 0.0f, 0.0f); // RGB: black
+
+    glVertex2i(x, y);
+    glVertex2i(x + width, y);
+    glVertex2i(x + width, y + height);
+    glVertex2i(x, y + height);
+  glEnd();
+}
+
+void OpenGLDrawer::drawMap(const Map& map) const {
+  std::vector<Room> rooms = map.getRooms();
+  std::vector<Hallway> hallways = map.getHallways();
+
+  for(const Room& room : rooms){
+      room.draw(*this);
+  }
+
+  for(const Hallway& hallway : hallways){
+    hallway.draw(*this);
+  }
+}
+
+void OpenGLDrawer::setTileSize(int tile_size){
+  this->tile_size = tile_size;
+}
---
+++ adapters/opengl_drawer.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../domain/interfaces/drawer.hpp"
+
+class OpenGLDrawer : public IDrawer {
+  public:
+    OpenGLDrawer();
+
+    void drawRoom(const Room& room) const override;
+    void drawHallway(const Hallway& hallway) const override;
+    void drawMap(const Map& map) const override;
+    void setTileSize(int tile_size) override;
+};
